ceb5297f331f45e8521b827cdf4fc3deb17b3575
Dobrica Stefan <sdobrica188@gmail.com>
2022-03-07 00:47:19 +0200

    Basic DXR shaders

--- src/Shaders/DXR/Basic.rt.hlsl
+++ src/Shaders/DXR/Basic.rt.hlsl
@@ -1,53 +1,53 @@
-/***************************************************************************
-# Copyright (c) 2018, NVIDIA CORPORATION. All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-#  * Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-#  * Redistributions in binary form must reproduce the above copyright
-#    notice, this list of conditions and the following disclaimer in the
-#    documentation and/or other materials provided with the distribution.
-#  * Neither the name of NVIDIA CORPORATION nor the names of its
-#    contributors may be used to endorse or promote products derived
-#    from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
-# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-# PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
-# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
-# OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-***************************************************************************/
 RaytracingAccelerationStructure gRtScene : register(t0);
 RWTexture2D<float4> gOutput : register(u0);
+
+struct RayPayload
+{
+    float3 color;
+};
 
 [shader("raygeneration")]
 void rayGen()
 {
     uint3 launchIndex = DispatchRaysIndex();
-    gOutput[launchIndex.xy] = float4(1.0f, 1.0f, 0.0f, 1.0f);
+    uint3 launchDims = DispatchRaysDimensions();
+
+    float2 crd = float2(launchIndex.xy);
+    float2 dims = float2(launchDims.xy);
+
+    float2 d = (crd / dims) * 2.0f - 1.0f;
+    float aspectRatio = dims.x / dims.y;
+
+    RayDesc ray;
+    ray.Origin = float3(0.0f, 0.0f, -3.0f);
+    ray.Direction = normalize(float3(d.x * aspectRatio, -d.y, 1));
+    ray.TMin = 0.0f;
+    ray.TMax = 10000.0f;
+
+    RayPayload payload;
+    TraceRay(gRtScene, 0, 0xFF, 0, 0, 0, ray, payload);
+
+    gOutput[launchIndex.xy] = float4(payload.color, 1.0f);
 }
 
-struct Payload
-{
-    bool hit;
-};
-
 [shader("miss")]
-void miss(inout Payload payload)
+void miss(inout RayPayload payload)
 {
-    payload.hit = false;
+    payload.color = float3(0.0f, 0.0f, 0.0f);
 }
 
 [shader("closesthit")]
-void chs(inout Payload payload, in BuiltInTriangleIntersectionAttributes attribs)
+void chs(inout RayPayload payload, in BuiltInTriangleIntersectionAttributes attribs)
 {
-    payload.hit = true;
+    float3 barycentrics = float3(
+        1.0f - attribs.barycentrics.x - attribs.barycentrics.y,
+        attribs.barycentrics.x,
+        attribs.barycentrics.y
+    );
+
+    float3 A = float3(1.0f, 0.0f, 0.0f);
+    float3 B = float3(0.0f, 1.0f, 0.0f);
+    float3 C = float3(0.0f, 0.0f, 1.0f);
+
+    payload.color = A * barycentrics.x + B * barycentrics.y + C * barycentrics.z;
 }
